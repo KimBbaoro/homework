6806360a83979986f091fd0d1b79edc57970874d
kimkunyou <bbaro1435@gmail.com>
2023-02-09 17:00:07 +0900

    0209_hw5

--- 0209.py
+++ 0209.py
@@ -1,15 +1,37 @@
-def mini(ary):
+import random
+import time
+
+def select(ary):
     n = len(ary)
-    for i in range(n-1):
+    for i in range(0,n-1):
         idx = i
-        for j in range(i+1,n):
-            if ary[idx] > ary[j]:
-                idx = j
+        for k in range(i+1, n):
+            if ary[idx] > ary[k]:
+                idx = k
+
         tmp = ary[i]
         ary[i] = ary[idx]
         ary[idx] = tmp
 
-for i in range(len(arr2)):
-    for j in range(lmne(arr2[i])):
-        arr1.append(arr2[i][j])
+    return ary
 
+def quick(arr,start, end):
+    if end <= start:
+        return
+    low = start
+    high = end
+
+    pivot = arr[(low + high) //2]
+    while low <=high:
+        while arr[low] < pivot:
+            low +=1
+        while arr[high] > pivot:
+            high -=1
+        if low <= high:
+            arr[low], arr[high] = arr[high], arr[low]
+            low+=1
+            high +=1
+    mid = low
+
+    quick(arr, start, mid-1)
+    quick(arr, mid, end)
